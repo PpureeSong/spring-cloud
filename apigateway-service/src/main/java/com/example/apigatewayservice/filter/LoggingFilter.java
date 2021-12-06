d9b7b2a6b64579aea360349bdc07e51f149f023e
Ppuree Song <roo0622@gmail.com>
2021-12-06 23:13:46 +0900

    Eureka 연동

--- apigateway-service/src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
@@ -22,24 +22,6 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
     @Override
     public GatewayFilter apply(Config config) {
         // Custom Pre Filter
-//        return ((exchange, chain) -> {
-//            ServerHttpRequest request = exchange.getRequest();
-//            ServerHttpResponse response = exchange.getResponse();
-//
-//            log.info("Global Filter baseMessage:{}", config.getBaseMessage());
-//
-//            if (config.isPreLogger()) {
-//                log.info("Global Filter Start: request id -> {}", request.getId());
-//            }
-//
-//            // Custom Post Filter
-//            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-//                if (config.isPostLogger()) {
-//                    log.info("Global Filter End: response code -> {}", response.getStatusCode());
-//                }
-//            }));
-//        });
-
         GatewayFilter filter = new OrderedGatewayFilter((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
