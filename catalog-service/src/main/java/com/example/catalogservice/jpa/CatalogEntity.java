01b0b5f9262eb970f007033e840e3e6b66dec55c
Ppuree Song <roo0622@gmail.com>
2021-12-12 23:16:15 +0900

    catalog-service 프로젝트 세팅 및 상품 목록 조회 개발

---
+++ catalog-service/src/main/java/com/example/catalogservice/jpa/CatalogEntity.java
@@ -0,0 +1,35 @@
+package com.example.catalogservice.jpa;
+
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "catalog")
+public class CatalogEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120, unique = true)
+    private String productId;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(nullable = false)
+    private Integer unitPrice;
+
+    @Column(nullable = false, updatable = false, insertable = false)
+    @ColumnDefault(value = "CURRENT_TIMESTAMP") //h2 db에서 현재 시간을 가져올 수 있는 함수
+    private Date createAt;
+
+}
