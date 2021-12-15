3ee3aa0698c2d3cffc1e6f6e672e4e8216977b6e
Ppuree Song <roo0622@gmail.com>
2021-12-15 22:54:14 +0900

    AuthenticationFilter 추가

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -11,9 +11,28 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll(); //인증없이 사용할 수 있음
+        http
+                .csrf()
+                .disable();
 
-        http.headers().frameOptions().disable(); //h2-console connect 시 프레임으로 나뉘어 있는 html 페이지 오류 해결 할 수 있음
+        http
+                .authorizeRequests()
+                .antMatchers("/**")
+                .hasIpAddress("192.168.0.11")
+                .and()
+                .addFilter(getAuthenticationFilter());
+
+        http
+                .headers()
+                .frameOptions()
+                .disable(); //h2-console connect 시 프레임으로 나뉘어 있는 html 페이지 오류 해결 할 수 있음
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
     }
 }
